cb051abe84209e9c623edb2230920a879db7d5c8
Vitor Henrique Carvalho de Morais <vhcmdev@gmail.com>
2024-11-21 13:17:37 -0300

    Add files via upload

---
+++ Questao 2 Prova 2/Pilha.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Pilha
+{
+    private Stack<string> pilhaDePecas = new Stack<string>();
+
+    public Pilha()
+    {
+        pilhaDePecas.Push("Hélice");
+        pilhaDePecas.Push("Suporte");
+        pilhaDePecas.Push("Hélice");
+        pilhaDePecas.Push("Cúpula de vidro");
+        pilhaDePecas.Push("Lâmpada");
+    }
+
+    public void ExibirPilha()
+    {
+        Console.WriteLine("Estado atual da pilha de peças:");
+        foreach (var peca in pilhaDePecas)
+        {
+            Console.WriteLine(peca);
+        }
+        Console.WriteLine();
+    }
+    public void TrocarPeca(string pecaVelha, string pecaNova)
+    {
+        Stack<string> pecasRemovidas = new Stack<string>();
+        bool encontrouPeca = false;
+
+        Console.WriteLine($"Iniciando troca da peça '{pecaVelha}' por '{pecaNova}'...\n");
+
+        while (pilhaDePecas.Count > 0)
+        {
+            string pecaAtual = pilhaDePecas.Pop();
+            if (pecaAtual == pecaVelha)
+            {
+                Console.WriteLine($"Peça encontrada: {pecaVelha}. Realizando a troca...\n");
+                pilhaDePecas.Push(pecaNova);
+                encontrouPeca = true;
+                break;
+            }
+            else
+            {
+                pecasRemovidas.Push(pecaAtual);
+            }
+        }
+
+        if (!encontrouPeca)
+        {
+            Console.WriteLine($"Peça '{pecaVelha}' não encontrada. A troca não foi realizada.\n");
+        }
+
+        while (pecasRemovidas.Count > 0)
+        {
+            pilhaDePecas.Push(pecasRemovidas.Pop());
+        }
+
+        Console.WriteLine("Troca realizada e peças remontadas.\n");
+    }
+}
---
+++ Questao 2 Prova 2/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Pilha ventilador = new Pilha();
+
+        // Exibe o estado inicial da pilha
+        ventilador.ExibirPilha();
+
+        // Realiza a troca da hélice quebrada por uma nova hélice
+        ventilador.TrocarPeca("Hélice", "Hélice nova");
+
+        // Exibe o estado da pilha após a troca
+        ventilador.ExibirPilha();
+    }
+}
